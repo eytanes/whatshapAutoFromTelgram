9835994285c5bb68c69538d62c91e5a20f262f87
eytanes <53444005+eytanes@users.noreply.github.com>
2020-06-24 19:32:11 +0300

    Update config.js

--- WhatsAppBotFromTelegram/config.js
+++ WhatsAppBotFromTelegram/config.js
@@ -1,22 +1,8 @@
 module.exports = {
     // API reference URL
-    apiUrl: "https://eu30.chat-api.com/instance126686/ ",
+    apiUrl: "https://eu30.chat-api.com/ // add your URl (from Chat-Api) ",
     // API token from your personal cabinet
-    token: "tc2olxe0fvsxz7xz",
+    token: "", // your token (from Chat-Api)
  
 }
 
-   /*"id": "false_972557272116-1589315034@g.us_3EB07A3EBA0276F64C9A",
-"body": "start",
-"fromMe": false,
-"self": 0,
-"isForwarded": 0,
-"author": "972586899203@c.us",
-"time": 1589315175,
-"chatId": "972557272116-1589315034@g.us",
-"messageNumber": 2,
-"type": "chat",
-"senderName": "moi",
-"quotedMsgBody": null,
-"quotedMsgId": null,
-"chatName": "test"*/
